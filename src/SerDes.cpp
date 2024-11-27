4f31f980194ca6155b8397cdc29f5deec6852892
ryomashita <86085692+ryomashita@users.noreply.github.com>
2024-11-27 09:38:43 +0900

    remove add & add case in SerDes

--- src/SerDes.cpp
+++ src/SerDes.cpp
@@ -3,7 +3,8 @@
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
-TEST(SerDes, Serialize) {
+TEST(SerDes, SerializeDeserialize) {
+  // 1. Serialize
   // 順序は保証されない
   // Ser/Des は UTF-8 エンコードのみに対応
   json j1 = {
@@ -11,9 +12,33 @@ TEST(SerDes, Serialize) {
       {"name", "Niels"},   {"nothing", {{"everything", 42}}},
       {"list", {1, 0, 2}},
   };
-  auto str = j1.dump();
+  const std::string str = j1.dump();
   std::cout << str << std::endl;
   EXPECT_EQ(
       str,
       R"({"happy":true,"list":[1,0,2],"name":"Niels","nothing":{"everything":42},"pi":3.141})");
+
+  // 2. Deserialize
+  try {
+    json j2 = json::parse(str);
+    EXPECT_EQ(j1, j2);
+  } catch (json::parse_error &e) {
+    FAIL() << e.what();
+  }
+}
+
+TEST(SerDes, DeserializeError) {
+  // 1. Deserialize
+  const std::string str =
+      R"({"happy":true,"list":[1,0,2],"name":"Niels","nothing":{"everything":42},"pi":3.141)";
+  try {
+    json j2 = json::parse(str);
+    FAIL() << "Expected an exception";
+  } catch (json::parse_error &e) {
+    std::cout << e.what() << std::endl;
+    // Exception message example:
+    // [json.exception.parse_error.101] parse error at line 1, column 83: syntax
+    // error while parsing object - unexpected end of input; expected '}'
+  }
+  SUCCEED();
 }
